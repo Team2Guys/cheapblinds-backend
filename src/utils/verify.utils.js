acedd0458c2e694ef082f04d66b5d88d4abfc12a
Sharjeel Faiq <mrmalik610@gmail.com>
2025-11-13 00:30:10 +0500

    added verify auth role util

--- src/utils/verify.utils.js
+++ src/utils/verify.utils.js
@@ -6,4 +6,10 @@ export const verifications = {
     if (!context.user) throw createError(401, "Unauthorized");
     return resolver(parent, args, context, info);
   },
+
+  verifyAuthRole: (roles) => (resolver) => async (parent, args, context, info) => {
+    if (!context.user) throw createError(401, "Unauthorized");
+    if (!roles.includes(context.user.role)) throw createError(403, "Forbidden");
+    return resolver(parent, args, context, info);
+  },
 };
