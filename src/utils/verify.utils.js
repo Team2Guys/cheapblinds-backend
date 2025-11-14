8bfe160230659fb1fa4bdbb8caf9e42bb46eda88
Sharjeel Faiq <sharjeelfaiq816@gmail.com>
2025-11-14 18:26:50 +0500

    updated verification error messages

--- src/utils/verify.utils.js
+++ src/utils/verify.utils.js
@@ -2,12 +2,12 @@ import createError from "http-errors";
 
 export const verifications = {
   verifyAccess: (resolver) => async (parent, args, context, info) => {
-    if (!context.user) throw createError(401, "Unauthorized");
+    if (!context.user) throw createError(401, "Unauthorized Access");
     return resolver(parent, args, context, info);
   },
 
   verifyRole: (roles) => (resolver) => async (parent, args, context, info) => {
-    if (!context.user) throw createError(401, "Unauthorized");
+    if (!context.user) throw createError(401, "Unauthorized Role");
     if (!roles.includes(context.user.role)) throw createError(403, "Forbidden");
     return resolver(parent, args, context, info);
   },
