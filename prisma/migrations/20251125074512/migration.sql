541593d7bb606c1a3fe0ac443ba61af8025f2ab0
Sharjeel Faiq <sharjeelfaiq816@gmail.com>
2025-11-26 17:55:15 +0500

    ran schema migrations

--- prisma/migrations/20251125074512/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `status` on table `Product` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Product" ALTER COLUMN "status" SET NOT NULL;
