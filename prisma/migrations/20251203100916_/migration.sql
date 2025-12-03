d3450f12e2e01f0c6f549be86540b67a51bd218b
Sharjeel Faiq <sharjeelfaiq816@gmail.com>
2025-12-03 15:19:52 +0500

    schema updated

--- prisma/migrations/20251203100916_/migration.sql
+++ prisma/migrations/20251203100916_/migration.sql
@@ -4,15 +4,24 @@ CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN', 'SUPER_ADMIN');
 -- CreateEnum
 CREATE TYPE "ContentStatus" AS ENUM ('DRAFT', 'PUBLISHED', 'ARCHIVED');
 
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('PENDING', 'PAID', 'CANCELED', 'FAILED', 'SHIPPED', 'COMPLETED');
+
+-- CreateEnum
+CREATE TYPE "PaymentStatus" AS ENUM ('FREE', 'PENDING', 'PAID', 'CANCELED', 'FAILED');
+
+-- CreateEnum
+CREATE TYPE "Permissions" AS ENUM ('ADD_PRODUCTS', 'EDIT_PRODUCTS', 'DELETE_PRODUCTS', 'ADD_CATEGORY', 'DELETE_CATEGORY', 'EDIT_CATEGORY', 'CHECK_PROFIT', 'CHECK_REVENUE', 'CHECK_VISITORS', 'VIEW_USERS', 'VIEW_SALES', 'VIEW_ADMINS', 'VIEW_TOTAL_PRODUCTS', 'VIEW_TOTAL_CATEGORIES');
+
 -- CreateTable
 CREATE TABLE "Admin" (
     "id" UUID NOT NULL,
     "name" VARCHAR(100) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
-    "permissions" TEXT[] DEFAULT ARRAY[]::TEXT[],
-    "lastEditedBy" VARCHAR(255),
+    "permissions" "Permissions"[] DEFAULT ARRAY[]::"Permissions"[],
     "role" "Role" NOT NULL DEFAULT 'ADMIN',
+    "lastEditedBy" VARCHAR(255),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -26,9 +35,9 @@ CREATE TABLE "User" (
     "lastName" VARCHAR(100) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
     "password" VARCHAR(255) NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
     "isEmailVerified" BOOLEAN NOT NULL DEFAULT false,
     "isNewsletterSubscribed" BOOLEAN NOT NULL DEFAULT false,
-    "role" "Role" NOT NULL DEFAULT 'USER',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -41,7 +50,7 @@ CREATE TABLE "Category" (
     "name" VARCHAR(100) NOT NULL,
     "description" TEXT,
     "shortDescription" VARCHAR(255),
-    "slug" VARCHAR(255),
+    "slug" VARCHAR(255) NOT NULL,
     "metaTitle" VARCHAR(70),
     "metaDescription" VARCHAR(160),
     "canonicalTag" VARCHAR(255),
@@ -59,17 +68,17 @@ CREATE TABLE "Category" (
 -- CreateTable
 CREATE TABLE "Subcategory" (
     "id" UUID NOT NULL,
+    "categoryId" UUID NOT NULL,
     "name" VARCHAR(100) NOT NULL,
     "description" TEXT,
     "shortDescription" VARCHAR(255),
-    "slug" VARCHAR(255),
+    "slug" VARCHAR(255) NOT NULL,
     "metaTitle" VARCHAR(70),
     "metaDescription" VARCHAR(160),
     "canonicalTag" VARCHAR(255),
     "breadcrumb" VARCHAR(255),
     "posterImageUrl" VARCHAR(512),
     "seoSchema" TEXT,
-    "categoryId" UUID NOT NULL,
     "lastEditedBy" VARCHAR(255),
     "status" "ContentStatus" NOT NULL DEFAULT 'PUBLISHED',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -81,6 +90,8 @@ CREATE TABLE "Subcategory" (
 -- CreateTable
 CREATE TABLE "Product" (
     "id" UUID NOT NULL,
+    "categoryId" UUID NOT NULL,
+    "subcategoryId" UUID NOT NULL,
     "name" VARCHAR(100) NOT NULL,
     "description" TEXT,
     "shortDescription" VARCHAR(255),
@@ -93,19 +104,17 @@ CREATE TABLE "Product" (
     "seoSchema" TEXT,
     "productImages" TEXT[] DEFAULT ARRAY[]::TEXT[],
     "price" DECIMAL(65,30) NOT NULL DEFAULT 0,
-    "discountPrice" DECIMAL(65,30) DEFAULT 0,
+    "discountPrice" DECIMAL(65,30) NOT NULL DEFAULT 0,
+    "motorPrice" DECIMAL(65,30) NOT NULL DEFAULT 0,
     "stock" INTEGER NOT NULL DEFAULT 0,
-    "height" DECIMAL(65,30) DEFAULT 0,
-    "width" DECIMAL(65,30) DEFAULT 0,
-    "weight" DECIMAL(65,30) DEFAULT 0,
+    "height" DECIMAL(65,30) NOT NULL DEFAULT 0,
+    "width" DECIMAL(65,30) NOT NULL DEFAULT 0,
     "color" VARCHAR(100),
     "pattern" VARCHAR(100),
     "composition" VARCHAR(255),
     "isMotorized" BOOLEAN NOT NULL DEFAULT false,
     "additionalInfo" TEXT,
     "measuringGuide" TEXT,
-    "categoryId" UUID NOT NULL,
-    "subcategoryId" UUID NOT NULL,
     "lastEditedBy" VARCHAR(255),
     "status" "ContentStatus" NOT NULL DEFAULT 'PUBLISHED',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -114,47 +123,42 @@ CREATE TABLE "Product" (
     CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
 );
 
--- CreateIndex
-CREATE UNIQUE INDEX "Admin_email_key" ON "Admin"("email");
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "firstName" VARCHAR(100) NOT NULL,
+    "lastName" VARCHAR(100) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "phone" VARCHAR(20) NOT NULL,
+    "country" VARCHAR(100) NOT NULL,
+    "state" VARCHAR(100) NOT NULL,
+    "city" VARCHAR(100) NOT NULL,
+    "address" TEXT NOT NULL,
+    "totalAmount" DECIMAL(65,30) NOT NULL DEFAULT 0,
+    "shippingCost" DECIMAL(65,30) NOT NULL DEFAULT 0,
+    "items" JSONB[],
+    "notes" TEXT,
+    "lastEditedBy" VARCHAR(255),
+    "paymentStatus" "PaymentStatus" NOT NULL DEFAULT 'PENDING',
+    "orderStatus" "OrderStatus" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateIndex
-CREATE INDEX "Admin_email_idx" ON "Admin"("email");
+CREATE UNIQUE INDEX "Admin_email_key" ON "Admin"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
-CREATE INDEX "User_email_idx" ON "User"("email");
-
--- CreateIndex
 CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
 
 -- CreateIndex
-CREATE INDEX "Category_name_idx" ON "Category"("name");
-
--- CreateIndex
-CREATE INDEX "Category_slug_idx" ON "Category"("slug");
-
--- CreateIndex
-CREATE INDEX "Subcategory_categoryId_idx" ON "Subcategory"("categoryId");
-
--- CreateIndex
-CREATE INDEX "Subcategory_slug_idx" ON "Subcategory"("slug");
-
--- CreateIndex
-CREATE INDEX "Product_name_idx" ON "Product"("name");
-
--- CreateIndex
-CREATE INDEX "Product_categoryId_idx" ON "Product"("categoryId");
-
--- CreateIndex
-CREATE INDEX "Product_subcategoryId_idx" ON "Product"("subcategoryId");
-
--- CreateIndex
-CREATE INDEX "Product_price_idx" ON "Product"("price");
-
--- CreateIndex
-CREATE INDEX "Product_slug_idx" ON "Product"("slug");
+CREATE UNIQUE INDEX "Category_slug_key" ON "Category"("slug");
 
 -- AddForeignKey
 ALTER TABLE "Subcategory" ADD CONSTRAINT "Subcategory_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -164,3 +168,6 @@ ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("cat
 
 -- AddForeignKey
 ALTER TABLE "Product" ADD CONSTRAINT "Product_subcategoryId_fkey" FOREIGN KEY ("subcategoryId") REFERENCES "Subcategory"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
