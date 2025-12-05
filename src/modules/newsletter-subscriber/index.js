44f25bab0aecc85c13e3b03bc2cad8128d0b578e
Sharjeel Faiq <sharjeelfaiq816@gmail.com>
2025-12-05 11:40:57 +0500

    NewsletterSubscriber API module and sendNewsLetterAPI  created

---
+++ src/modules/newsletter-subscriber/index.js
@@ -0,0 +1,2 @@
+export { newsletterSubscriberTypeDefs } from "./newsletter-subscriber.typeDefs.js";
+export { newsletterSubscriberResolvers } from "./newsletter-subscriber.resolvers.js";
---
+++ src/modules/newsletter-subscriber/newsletter-subscriber.typeDefs.js
@@ -0,0 +1,27 @@
+import { gql } from "graphql-tag";
+
+export const newsletterSubscriberTypeDefs = gql`
+  input CreateNewsletterSubscriberInput {
+    email: String!
+    isActive: Boolean!
+  }
+
+  input UpdateNewsletterSubscriberByIdInput {
+    email: String
+    isActive: Boolean
+  }
+
+  type Query {
+    newsletterSubscriberList: [NewsletterSubscriber!]!
+
+    newsletterSubscriberById(id: ID!): NewsletterSubscriber
+  }
+
+  type Mutation {
+    createNewsletterSubscriber(input: CreateNewsletterSubscriberInput!): NewsletterSubscriber
+    updateNewsletterSubscriberById(
+      id: ID!
+      input: UpdateNewsletterSubscriberByIdInput!
+    ): NewsletterSubscriber
+  }
+`;
