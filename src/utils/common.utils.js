95f94d3fb724fcb3280a412be653804154cb9dde
Sharjeel Faiq <mrmalik610@gmail.com>
2025-12-10 19:51:28 +0500

    code refactored

--- src/utils/common.utils.js
+++ src/utils/common.utils.js
@@ -14,19 +14,5 @@ export const commonUtils = {
       } catch (error) {
         throw createError(error.status || 500, error.message);
       }
-    },
-
-  /**
-   * Validate UUID
-   * @param {string} id - UUID
-   * @returns {void}
-   */
-  validateUuid: (id) => {
-    if (!id) throw createError(400, 'Invalid Uuid.');
-
-    const uuidRegex =
-      /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-
-    if (!uuidRegex.test(id)) throw createError(400, 'Invalid Uuid.');
-  }
+    }
 };
