be15111d3daee8c1b89611d265b34d1b50e633f5
Sharjeel Faiq <sharjeelfaiq816@gmail.com>
2026-01-16 09:44:20 +0500

    graphql schema refactored

--- src/api/inquiry/inquiry.typeDef.js
+++ src/api/inquiry/inquiry.typeDef.js
@@ -1,6 +1,33 @@
 import { gql } from 'graphql-tag';
 
 export const inquiryTypeDefs = gql`
+  enum InquiryType {
+    EMAIL
+    PHONE
+    WHATSAPP
+    OTHER
+  }
+
+  enum InquiryStatus {
+    NEW
+    READ
+    RESOLVED
+  }
+
+  type Inquiry {
+    id: ID!
+
+    name: String!
+    email: String!
+    phone: String!
+    message: String
+    inquiryType: InquiryType!
+    inquiryStatus: InquiryStatus!
+
+    createdAt: DateTime!
+    updatedAt: DateTime!
+  }
+
   input CreateInquiryInput {
     name: String!
     email: String!
